0475640dd05fee35bcddd59292694595ee115611
Noer Paanakker <noer@lightworksweb.nl>
2021-02-09 15:58:31 +0100

    created basic components for app (training, auth, welcome), added material ui

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,9 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SignupComponent, LoginComponent, TrainingComponent, WelcomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
